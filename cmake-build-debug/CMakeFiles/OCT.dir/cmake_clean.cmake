bca539562d362f3d0e3f639bcee3db19fc5f4f43
Bruno Bartolomasi <brunobrt@hotmail.com>
2021-06-08 17:14:32 -0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/OCT.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OCT.dir/main.cpp.obj"
+  "OCT.exe"
+  "OCT.exe.manifest"
+  "OCT.pdb"
+  "libOCT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OCT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
